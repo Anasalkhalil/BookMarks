1c1078021f5c910f097bedb9e08489be127b1ca1
Anas Alkhalil <anasalkhalel@hotmail.com>
2018-07-20 16:48:04 +0200

    final

--- js/main.js
+++ js/main.js
@@ -4,6 +4,19 @@ function saveBookmark(e){
     siteName = document.getElementById('siteName').value;
     siteURL = document.getElementById('siteURL').value;
     
+    var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+    var regex = new RegExp(expression);
+    
+    if(!siteURL || !siteURL){
+        alert('Please Enter The Field Values!..');
+        return false;
+    }
+
+    if(!siteURL.match(regex)){
+        alert('Please Enter Right Site URL');
+        document.getElementById('siteUrl').focus();
+        return flase;
+    }
     var bookmark = {
         name : siteName,
         value : siteURL
@@ -25,7 +38,10 @@ function saveBookmark(e){
         localStorage.setItem('bookmarks',JSON.stringify(bookmarks));
     }
     e.preventDefault();
+    fetchBookmarks();
 }
+
+
 
 function fetchBookmarks(){
     var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
@@ -39,10 +55,24 @@ function fetchBookmarks(){
 
         bookmarksResult.innerHTML += '<div class="well">' +
                                      '<h3>'+ name + '</h3>' +
-                                     '<a class = "btn btn-danger" target="_blank" href="'+ value +'">' +
+                                     '<a class="btn btn-primary" target="_blank" href="'+ value +'">' +
                                      'Visit' + 
+                                     '</a>' +
+                                     ' <a class="btn btn-danger" onclick="deleteBookark(\''+value+'\')" href="#">' +
+                                     'Delete' + 
                                      '</a>'
                                      '</div>';
     }
     
+}
+
+function deleteBookark(url){
+    var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+    for(var i=0; i<bookmarks.length ; i++){
+        if(bookmarks[i].value == url){
+            bookmarks.splice(i,1);
+        }
+    }
+    localStorage.setItem('bookmarks',JSON.stringify(bookmarks));
+    fetchBookmarks();
 }
